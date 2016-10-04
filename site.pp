e4994ce11615d63ad286b9ff33cc5996c1ab4bdd
evbr1432 <evbr1432@colorado.edu>
2016-10-04 15:38:47 -0400

    Working User mscott

--- site.pp
+++ site.pp
@@ -1,30 +1,30 @@
-file { '/home/mscott/skel':
-	path => "/etc/skel",
-	source => 'puppet:/etc/skel/'
-	group => 'mscott',
-	recurse => true,
-}
-file { '/tmp/testdir':
-	ensure => "directory",
-	owner => 'root',
-	group => 'wheel',
-	mode => '2755',
-}
+#file { '/home/mscott/skel':
+#	path => "/etc/skel",
+#	source => 'puppet:/etc/skel/'
+#	group => 'mscott',
+#	recurse => true,
+#}
+#file { '/home/mscott':
+#	ensure => "directory",
+#	owner => 'mscott',
+#	group => 'mscott',
+#	mode => '2755',
+#}
 group {"managers":
 	ensure => "present",
-	gid => "5000",
+	gid => "5100",
 }
 group {"sales":
 	ensure => "present",
-	gid => "5001"
+	gid => "5101",
 }
 group {"accounting":
-	ensure => "present"
-	gid => 5002"
-"
+	ensure => "present",
+	gid => "5102",
+}
 group {"mscott":
-	ensure => "present"
-	gid => "4001"
+	ensure => "present",
+	gid => "4001",
 }
 
 user { "mscott":
@@ -32,9 +32,15 @@ user { "mscott":
 	ensure => present,
 	comment => "Michal Scott",
 	gid => "4001",
-	groups => ["manager","sales","accounting"],
+	groups => ["managers","sales","accounting"],
 	membership => minimum,
 	shell => "/bin/bash",
 	home => "/home/mscott",
 	managehome => true,
+}
+file { '/home/mscott':
+	ensure => "directory",
+	owner => 'mscott',
+	group => 'mscott',
+	mode => '2755',
 }
